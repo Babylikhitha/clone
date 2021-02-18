6d3a2b01a78a801d9c52ef3e8c88a1469f9de8ad
Babylikhitha <79193496+Babylikhitha@users.noreply.github.com>
2021-02-18 16:53:22 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -18,8 +18,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'E-commerce project using Java'
-copyright = '2020, Harideep'
-author = 'Harideep'
+copyright = '2021, Babylikhitha'
+author = 'Babylikhitha'
 
 # The full version, including alpha/beta/rc tags
 release = ''
